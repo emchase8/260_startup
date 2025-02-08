85f047389f9ae426ac2a457bc3264fa3a0549e51
emchase8 <emchase8@byu.edu>
2025-02-07 17:30:25 -0700

    fixing links on app.jsx

--- startup-files/src/app.jsx
+++ startup-files/src/app.jsx
@@ -4,7 +4,7 @@ import './app.css';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
 import { Game } from './game/game';
-import { Score } from './score/scoreboard';
+import { Score } from './score/score';
 import { Info } from './info/info';
 
 export default function App() {
@@ -21,16 +21,24 @@ export default function App() {
                         <div className="collapse navbar-collapse" id="navbarNav">
                             <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="index.html">Home</a>
+                                <NavLink className='nav-link' to=''>
+                                    Login
+                                </NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="game.html">Game</a>
+                                <NavLink className='nav-link' to='game'>
+                                    Game
+                                </NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="scoreboard.html">Scoreboard</a>
+                                <NavLink className='nav-link' to='score'>
+                                    Score
+                                </NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="info.html">About</a>
+                                <NavLink className='nav-link' to='info'>
+                                    Info
+                                </NavLink>
                             </li>
                             </ul>
                         </div>
@@ -38,7 +46,13 @@ export default function App() {
                 </nav>
             </header>
     
-            <main>App components go here</main>
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/game' element={<Game />} />
+                <Route path='/score' element={<Score />} />
+                <Route path='/info' element={<Info />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
     
             <footer>
                 <div className="footer_colors">Emily Chase <a href="https://github.com/emchase8/260_startup">Github</a></div>
@@ -48,41 +62,6 @@ export default function App() {
     );
   }
 
-export default function App() {
-    return (
-      <div>
-        <header>
-            <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-                <div className="container-fluid">
-                    <a className="navbar-brand">Hang Cheese</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                          <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="index.html">Home</a>
-                          </li>
-                          <li className="nav-item">
-                            <a className="nav-link" href="game.html">Game</a>
-                          </li>
-                          <li className="nav-item">
-                            <a className="nav-link" href="scoreboard.html">Scoreboard</a>
-                          </li>
-                          <li className="nav-item">
-                            <a className="nav-link" href="info.html">About</a>
-                          </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-  
-        <main>App components go here</main>
-  
-        <footer>
-            <div className="footer_colors">Emily Chase <a href="https://github.com/emchase8/260_startup">Github</a></div>
-        </footer>
-      </div>
-    );
-  }
+function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+}
