ddb8e27e4f88fc718ec660c63afacdde0bbc89fc
emchase8 <emchase8@byu.edu>
2025-02-20 11:12:42 -0700

    mapping out path

--- startup-files/src/game/game.jsx
+++ startup-files/src/game/game.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { use, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import './game.css';
 import '../app.css';
 import {Players} from './players'
 
 export function Game(props) {
+  const [image, setImage] = useState("starting_image.png"); //image updator
+  const [guess, setGuess] = useState([]); //all guesses
+  const [wrong_guess, setWrongGuess] = useState([]); //wrong guesses
+
+  const handleStepUp = (event) => [
+    setImage(image++)  //have js to also update wrong_guesses and if statement to change image?
+  ]
+  
+
   return (
     <main className="container-fluid text-center">
       <div className="section">
           <Players username={props.username}/>
           <br />
-          <img src="starting_image.png" alt="game image" className="responsive"></img>
+          {/* image now reactive */}
+          <img src={{image}} alt="game image" className="responsive"></img> 
       </div>
       <br />
       <div className="section">
+          {/* function to update wrong list to be above */}
           <div className="guess guesses">Wrong Guesses
               <ul>
                   <li>Wrong guess one</li>
@@ -27,9 +38,12 @@ export function Game(props) {
                   <span className="input-group-text">Guess: </span>
                   <input className="form-control" type="text" placeholder="type your guess here" />
               </div>
+              {/* follow same pattern as login to catch input and use it */}
               <Button className="btn btn-warning" type="submit" style={{marginBottom: '1em'}}>Submit Guess</Button>
+              {/* how the heck do i do this? */}
               <Button className="btn btn-secondary" type="submit">Restart Game</Button>
           </div>
+          {/* function to update word above */}
           <div className="guess guesses">Word: <span className="word">__ __ __ __ __ __</span> (3rd party word generator will be used, if I can figure that out)</div>
       </div>
     </main>
