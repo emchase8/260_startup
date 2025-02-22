90213b87ee65b095937575e52ab41946cc0ef76b
emchase8 <emchase8@byu.edu>
2025-02-22 11:09:34 -0700

    figuring out the image

--- startup-files/src/game/game.jsx
+++ startup-files/src/game/game.jsx
@@ -7,29 +7,15 @@ import {Players} from './players'
 export function Game(props) {
   // choose random word in const[word, setWord] thing that can be reset on reset but SHOULD NOT change otherwise
   
-  const [image, setImage] = useState("starting_image.png"); //image updator
+  const [image, setImage] = useState("new_starting_image.png"); //image updator
   const [current_guess, setCurrentGuess] = useState('');
   const [right_guesses, setRightGuesses] = useState([]); //all guesses
   const [wrong_guesses, setWrongGuesses] = useState([]); //wrong guesses
-  const [word, setWord] = useState([]) //the actual word, need to set that somehow
+  const [word, setWord] = useState(['m', 'a', 'n', 'd', 'o', 's']) //the actual word, need to set that somehow
   const [display_word, setDisplayWord] = useState("__ __ __ __ __ __")
 
-  function handleWrongGuess() {
-    if (wrong_guesses.length == 1) {
-      setImage("one_wrong.png");
-    } else if (wrong_guesses.length == 2) {
-      setImage("two_wrong.png");
-    } else if (wrong_guesses.length == 3) {
-      setImage("three_wrong.png");
-    } else if (wrong_guesses.length == 4) {
-      setImage("four_wrong.png");
-    } else {
-      setImage("last_guess.png");
-    }
-  }
-
   async function reset() {
-    setImage("starting_image.png");
+    setImage("new_starting_image.png");
     setCurrentGuess('');
     setRightGuesses([]);
     setWrongGuesses([]);
@@ -39,44 +25,38 @@ export function Game(props) {
 
   async function handle_guess() {
     if (word.includes(current_guess)) {
-      right_guesses.push(current_guess);
+      setRightGuesses((prev) => [...prev, current_guess]);
+      let new_display_word = display_word.split(' ')
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] === current_guess) {
+          new_display_word[i] = current_guess;
+        }
+      }
+      setDisplayWord(new_display_word.join(' '));
       //somehow print out updated word, check if word has been completed
     } else {
-      wrong_guesses.push(current_guess);
-      handleWrongGuess();
+      setWrongGuesses((prev) => 
+        {const temp_guesses = [...prev, current_guess]; 
+          handleWrongGuess(temp_guesses); 
+          return temp_guesses});
+    }
+    setCurrentGuess('');
+  }
+
+  async function handleWrongGuess(wrong_guesses) {
+    console.log(wrong_guesses.length);
+    if (wrong_guesses.length === 1) {
+      setImage("one_wrong.png");
+    } else if (wrong_guesses.length === 2) {
+      setImage("two_wrong.png");
+    } else if (wrong_guesses.length === 3) {
+      setImage("three_wrong.png");
+    } else if (wrong_guesses.length === 4) {
+      setImage("four_wrong.png");
+    } else if (wrong_guesses.length > 4) {
+      setImage("last_guess.png");
     }
   }
-  
-  // main game written by chat
-  // async function handleGuess() {
-  //   if (word.includes(currentGuess)) {
-  //     // If the guess is correct, update right guesses
-  //     setRightGuesses((prev) => [...prev, currentGuess]);
-
-  //     // Update the displayed word (e.g., replace blanks with the correct letter)
-  //     let newDisplayedWord = displayedWord.split(' ');
-  //     for (let i = 0; i < word.length; i++) {
-  //       if (word[i] === currentGuess) {
-  //         newDisplayedWord[i] = currentGuess;
-  //       }
-  //     }
-  //     setDisplayedWord(newDisplayedWord.join(' '));
-
-  //     // Check if the word is complete
-  //     if (newDisplayedWord.join('') === word) {
-  //       console.log('You have completed the word!');
-  //       // Optionally show a message or reset the game here
-  //     }
-
-  //   } else {
-  //     // If the guess is incorrect, update wrong guesses and handle the wrong guess logic
-  //     setWrongGuesses((prev) => [...prev, currentGuess]);
-  //     handleWrongGuess();
-  //   }
-
-  //   setCurrentGuess(''); // Clear the input after the guess
-  // }
-
 
   return (
     <main className="container-fluid text-center">
@@ -84,7 +64,7 @@ export function Game(props) {
           <Players username={props.username}/>
           <br />
           {/* image now reactive */}
-          <img src={{image}} alt="game image" className="responsive"></img> 
+          <img src={image} alt="game image" className="responsive"></img>
       </div>
       <br />
       <div className="section">
@@ -102,7 +82,7 @@ export function Game(props) {
               {/* follow same pattern as login to catch input and use it */}
               <Button className="btn btn-warning" type="submit" style={{marginBottom: '1em'}} onClick={() => handle_guess()}>Submit Guess</Button>
               {/* how the heck do i do this? probably just a function to reset everything to be above*/}
-              <Button className="btn btn-secondary" type="submit" onClick={() => reset()} disabled={wrong_guesses.length = 0}>Restart Game</Button>
+              <Button className="btn btn-secondary" type="submit" onClick={() => reset()} disabled={!wrong_guesses.length}>Restart Game</Button>
           </div>
           {/* function to update word will be above */}
           <div className="guess guesses">Word: <span className="word">{display_word}</span> (3rd party word generator will be used, if I can figure that out)</div>
