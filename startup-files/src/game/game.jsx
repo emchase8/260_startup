e631376804f806f50c123ce2df77e1f71ab76e1d
emchase8 <emchase8@byu.edu>
2025-02-22 21:39:45 -0700

    game now fully works

--- startup-files/src/game/game.jsx
+++ startup-files/src/game/game.jsx
@@ -8,7 +8,7 @@ import {GameEvent, GameNotify} from './GameNotify';
 export function Game(props) {
   const word_choices = ['ACCEPT', 'BRIDGE', 'CHANCE', 'DURING', 'EITHER', 'FABRIC', 'GAINED', 'HACKED', 'IGLOOS', 'JABBED', 'KABALA', 'LABELS', 'MACAWS', 'NACHOS', 'OBLIGE', 'PACIFY', 'QUAILS', 'RAFTER', 'SABBAT', 'TABLES', 'UGLIER', 'VACANT', 'WACKOS', 'XENIAL', 'YACHTS', 'ZAFFER']
   
-  async function choose_word(word_choices) {
+  function choose_word(word_choices) {
     const i = Math.floor(Math.random() * word_choices.length);
     const choosen_word = word_choices[i];
     const letter_list = [];
@@ -24,6 +24,7 @@ export function Game(props) {
   const [wrong_guesses, setWrongGuesses] = useState([]); 
   const [word, setWord] = useState(choose_word(word_choices)) 
   const [display_word, setDisplayWord] = useState("__ __ __ __ __ __")
+  const [updated_word, setUpdatedWord] = useState(display_word);
   //SCORES WOMAN, FIGURE OUT HOW THOSE WORK
 
   async function reset() {
@@ -36,19 +37,24 @@ export function Game(props) {
   }
 
   async function handle_guess() {
-    if (word.includes(current_guess)) {
-      setRightGuesses((prev) => [...prev, current_guess]);
+    if (word.includes(current_guess.toUpperCase())) {
+      setRightGuesses((prev) => [...prev, current_guess.toUpperCase()]);
       let new_display_word = display_word.split(' ')
       for (let i = 0; i < word.length; i++) {
-        if (word[i] === current_guess) {
-          new_display_word[i] = current_guess;
+        if (word[i] === current_guess.toUpperCase()) {
+          new_display_word[i] = current_guess.toUpperCase();
         }
       }
-      setDisplayWord(new_display_word.join(' '));
-      //somehow print out updated word, check if word has been completed
-    } else {
+      const updated_word = new_display_word.join(' ');
+      setDisplayWord(updated_word);
+      setUpdatedWord(updated_word);
+      if (!updated_word.includes('__')) {
+        console.log('you won!');
+        setImage('you_won.png');
+      }
+    } else if (current_guess != '') {
       setWrongGuesses((prev) => 
-        {const temp_guesses = [...prev, current_guess]; 
+        {const temp_guesses = [...prev, current_guess.toUpperCase()]; 
           handleWrongGuess(temp_guesses); 
           return temp_guesses});
     }
@@ -75,12 +81,10 @@ export function Game(props) {
       <div className="section">
           <Players username={props.username}/>
           <br />
-          {/* image now reactive */}
           <img src={image} alt="game image" className="responsive"></img>
       </div>
       <br />
       <div className="section">
-          {/* function to update wrong list to be above */}
           <div className="guess guesses">Wrong Guesses
               <ul>
                   {wrong_guesses.map((w_guess, index) => (<li key = {index}>{w_guess}</li>))}
@@ -91,12 +95,9 @@ export function Game(props) {
                   <span className="input-group-text">Guess: </span>
                   <input className="form-control" type="text" value={current_guess} onChange={(e) => setCurrentGuess(e.target.value)} placeholder="type your guess here" />
               </div>
-              {/* follow same pattern as login to catch input and use it */}
-              <Button className="btn btn-warning" type="submit" style={{marginBottom: '1em'}} onClick={() => handle_guess()}>Submit Guess</Button>
-              {/* how the heck do i do this? probably just a function to reset everything to be above*/}
-              <Button className="btn btn-secondary" type="submit" onClick={() => reset()} disabled={!wrong_guesses.length}>Restart Game</Button>
+              <Button className="btn btn-warning" type="submit" style={{marginBottom: '1em'}} onClick={() => handle_guess()} disabled={!updated_word.includes('__')}>Submit Guess</Button>
+              <Button className="btn btn-secondary" type="submit" onClick={() => reset()} disabled={!wrong_guesses.length || !right_guesses.length}>Restart Game</Button>
           </div>
-          {/* function to update word will be above */}
           <div className="guess guesses">Word: <span className="word">{display_word}</span> (3rd party word generator will be used, if I can figure that out)</div>
       </div>
     </main>
