1a6a5a3f250d89fd48bc940d060ba81a5cc807a6
emchase8 <emchase8@byu.edu>
2025-03-22 15:10:13 -0600

    database.js setup

---
+++ startup-files/service/database.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+
+const client = new MongoClient(url);
+const db = client.db('startup');
+const user_collection = db.collection('user');
+const score_collection = db.collection('scores');
+
+//testing connection so hopefully things don't break
+(async function testConnection() {
+    try {
+      await db.command({ ping: 1 });
+    } catch (ex) {
+      console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+      process.exit(1);
+    }
+  })();
